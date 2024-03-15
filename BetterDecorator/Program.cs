4c7093d1cf1a26c6722261fc0f6ad4e178ac8f19
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-15 11:25:59 +0000

    BetterDecorator project added to sln

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decor("Good Morning!", '*'));
+            Console.WriteLine(Decor("Good Evening!", '.'));
+        }
+
+        private static string Decor(string s, char dec)
+        {
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+        }
+    }
+}
