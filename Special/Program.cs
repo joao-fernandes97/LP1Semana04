fa11194339b753df6cfdadf8bcf71c23e564db01
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-15 12:04:02 +0000

    Recursive GetSpecial method and Main logic

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,21 @@ namespace Special
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(n));
+        }
+
+        private static int GetSpecial(int n)
+        {
+            int seq;
+            if(n==0)
+                seq = 0;
+            else if(n==1)
+                seq = 1;
+            else 
+                seq = GetSpecial(n-1) + 2*GetSpecial(n-2);
+            return seq;
         }
     }
 }
